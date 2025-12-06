6f05dead437dc8cd13c9db6503d4ae609253b752
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2025-12-06 01:34:31 +0000

    fix cuando se cierra, evita cerrar toda la aplicacion

--- EcoRutas/src/vista/VistaHorarioRec.java
+++ EcoRutas/src/vista/VistaHorarioRec.java
@@ -37,7 +37,7 @@ public class VistaHorarioRec extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         tablaHorarios = new javax.swing.JTable();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jLabel1.setText("Horario de Recolección");
