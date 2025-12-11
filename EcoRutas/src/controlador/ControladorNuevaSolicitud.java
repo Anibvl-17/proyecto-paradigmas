851c79ab0b04b12e95c0a143c0f304088428e5a1
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2025-12-11 21:16:59 +0000

    add: funciones agregar y limpiar

--- EcoRutas/src/controlador/ControladorNuevaSolicitud.java
+++ EcoRutas/src/controlador/ControladorNuevaSolicitud.java
@@ -1,15 +1,18 @@
 package controlador;
 
-import modelo.GestorSolicitudes;
-import vista.VistaNuevaSolicitud;
+import modelo.*;
+import vista.*;
 
 public class ControladorNuevaSolicitud {
     GestorSolicitudes modelo;
     VistaNuevaSolicitud vista;
     
+    VistaMensajes vistaMensajes;
+    
     public ControladorNuevaSolicitud(GestorSolicitudes modelo, VistaNuevaSolicitud vista) {
         this.vista = vista;
         this.modelo = modelo;
+        vistaMensajes = new VistaMensajes();
     }
     
     public void iniciar() {
@@ -18,8 +21,44 @@ public class ControladorNuevaSolicitud {
         
         vista.setVisible(true);
         
-        // Definir botones con sus respectivas funciones
+        vista.getBtnCrear().addActionListener(e -> agregarSolicitud());
+        vista.getBtnLimpiar().addActionListener(e -> limpiarFormulario());
     }
     
-    // Agregar funciones
+    public void agregarSolicitud() {
+        String nombre = vista.getTxtNombre().getText();
+        String tipoSolicitud = (String) vista.getComboBoxTipo().getSelectedItem();
+        String mensaje = vista.getTxtMensaje().getText();
+        
+        try {
+            modelo.agregarSolicitud(new Solicitud(calcularId(), nombre, mensaje, tipoSolicitud));
+            vistaMensajes.mostrarInfo(null, "Solicitud creada exitosamente");
+        } catch (IllegalArgumentException e) {
+            vistaMensajes.mostrarError(null, e.getMessage());
+        }
+    }
+    
+    private void limpiarFormulario() {
+        // Obtiene el primer item del combo box
+        Object primerItem = vista.getComboBoxTipo().getItemAt(0);
+        
+        // Selecciona el primer item de los combo box
+        vista.getComboBoxTipo().getModel().setSelectedItem(primerItem);
+        
+        // Limpia el texto de los textfields
+        vista.getTxtNombre().setText("");
+        vista.getTxtMensaje().setText("");
+        vista.getTxtNombre().requestFocus();
+    }
+    
+    // Calcula la id de la solicitud segun la última solicitud de la lista.
+    private int calcularId() {
+        int cantidadHorarios = modelo.listarSolicitudes().size();
+
+        if (cantidadHorarios == 0) {
+            return 1;
+        }
+
+        return modelo.listarSolicitudes().get(cantidadHorarios - 1).getId() + 1;
+    }
 }
