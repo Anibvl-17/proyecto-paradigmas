ae98a3c846e1e40ebeb55829ca137bd148e318dc
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2025-12-14 17:48:04 +0000

    agregar controlador de ver contenedores

---
+++ EcoRutas/src/controlador/ControladorContenedor.java
@@ -0,0 +1,45 @@
+package controlador;
+
+import java.io.IOException;
+import javax.swing.table.DefaultTableModel;
+import modelo.*;
+import vista.*;
+
+public class ControladorContenedor {
+    private GestorContenedor modelo;
+    private VistaContenedores vista;
+    
+    private VistaMensajes vistaMensajes;
+    
+    public ControladorContenedor(GestorContenedor modelo, VistaContenedores vista) {
+        this.modelo = modelo;
+        this.vista = vista;
+        vistaMensajes = new VistaMensajes();
+    }
+    
+    public void iniciar() {
+        vista.pack();
+        vista.setLocationRelativeTo(null);
+        
+        listarContenedores();
+        
+        vista.setVisible(true);
+    }
+    
+    private void listarContenedores() {
+        try {
+            modelo.cargarArchivo();
+            
+            DefaultTableModel m = (DefaultTableModel) vista.getTablaContenedores().getModel();
+            m.setNumRows(0);
+            
+            for (Contenedor c : modelo.listarContenedores()) {
+                m.addRow(new Object[]{c.getTipo(), c.getEstado(), c.getColor(), c.getCapacidadMaxima(), c.getCapacidadActual()});
+            }
+            
+        } catch (IOException e) {
+            vistaMensajes.mostrarError(null, "Error: No se pudo cargar los contenedores");
+        }
+    }
+    
+}
