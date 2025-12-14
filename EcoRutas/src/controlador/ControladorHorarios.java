e46783794c4e52eea59003629a644d9b537be8a5
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2025-12-14 20:18:45 +0000

    carga horarios al iniciar vista

--- EcoRutas/src/controlador/ControladorHorarios.java
+++ EcoRutas/src/controlador/ControladorHorarios.java
@@ -1,5 +1,8 @@
 package controlador;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import javax.swing.table.DefaultTableModel;
 import vista.*;
 import modelo.*;
 
@@ -7,9 +10,12 @@ public class ControladorHorarios {
     VistaHorarioRec vista;
     GestorHorarioRecoleccion modelo;
     
+    VistaMensajes vistaMensajes;
+    
     public ControladorHorarios(GestorHorarioRecoleccion modelo, VistaHorarioRec vista) {
         this.vista = vista;
         this.modelo = modelo;
+        vistaMensajes = new VistaMensajes();
     }
     
     public void iniciar() {
@@ -17,5 +23,31 @@ public class ControladorHorarios {
         vista.setLocationRelativeTo(null);
         
         vista.setVisible(true);
+        
+        cargarHorarios();
     }
+    
+    public void listarHorarios() {
+        DefaultTableModel m = (DefaultTableModel) vista.getTablaHorarios().getModel();
+        m.setNumRows(0);
+        
+        for (HorarioRecoleccion h : modelo.listarHorarios()) {
+            m.addRow(new Object[] {h.getSector(), h.getDiaSemana(), h.getHoraInicio(), h.getHoraFin(), h.getTipoResiduo()});
+        }
+    }
+    
+    public void cargarHorarios() {
+        try {
+            modelo.cargarArchivo();
+            listarHorarios();
+        } catch (FileNotFoundException e) {
+            // No hay horarios guardados
+        } catch (IOException e) {
+            vistaMensajes.mostrarError(null, "Error: No se pudo cargar los horarios");
+        }
+    }
+    
+    // Pendiente
+    // - Filtrar por sector (y por tipo?)
+    // - Listar todos
 }
